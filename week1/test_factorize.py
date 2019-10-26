d4e8ba02ef506d47269dfd4f87c7106a8c566f81
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-26 20:59:27 +0600

    Changed i to x

--- week1/test_factorize.py
+++ week1/test_factorize.py
@@ -3,46 +3,46 @@ class TestFactorize(unittest.TestCase):
         """
         Test that float and string types raise exception
         """
-        for i in 1.5, 'string':
-            with self.subTest(i=i):
-                self.assertRaises(TypeError, factorize, i)
+        for x in 1.5, 'string':
+            with self.subTest(x=x):
+                self.assertRaises(TypeError, factorize, x)
     
     def test_negative(self):
         """
         Test that negative argument raises exception
         """
-        for i in -1, -10, -100:
-            with self.subTest(i=i):
-                self.assertRaises(ValueError, factorize, i)
+        for x in -1, -10, -100:
+            with self.subTest(x=x):
+                self.assertRaises(ValueError, factorize, x)
     
     def test_zero_and_one_cases(self):
         """
         Test that 0 and 1 gets factors (0,) and (1,)
         """
-        for i, ans in (0, (0,)), (1, (1,)):
-            with self.subTest(i=i):
-                self.assertEqual(factorize(i), ans)
+        for x, ans in (0, (0,)), (1, (1,)):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), ans)
     
     def test_simple_numbers(self):
         """
         Test that factors of prime numbers are prime numbers itself in tuple
         """
-        for i, ans in (3, (3,)), (13, (13,)), (29, (29,)):
-            with self.subTest(i=i):
-                self.assertEqual(factorize(i), ans)
+        for x, ans in (3, (3,)), (13, (13,)), (29, (29,)):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), ans)
     
     def test_two_simple_multipliers(self):
         """
         Test that even and squared numbers have exactly two factors in tuple
         """
-        for i, ans in (6, (2,3)), (26, (2,13)), (121, (11,11)):
-            with self.subTest(i=i):
-                self.assertEqual(factorize(i), ans)
+        for x, ans in (6, (2,3)), (26, (2,13)), (121, (11,11)):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), ans)
     
     def test_many_multipliers(self):
         """
         Test that factorize returns more than two factors in tuple
         """
-        for i, ans in (1001, (7,11,13)), (9699690, (2,3,5,7,11,13,17,19)):
-            with self.subTest(i=i):
-                self.assertEqual(factorize(i), ans)
+        for x, ans in (1001, (7,11,13)), (9699690, (2,3,5,7,11,13,17,19)):
+            with self.subTest(x=x):
+                self.assertEqual(factorize(x), ans)
