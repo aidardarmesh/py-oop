89cfc57cff31cbe9d43057c856a984f6ef788c6f
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-10-29 22:52:15 +0600

    Implemented all decorators

--- week3/decorator.py
+++ week3/decorator.py
@@ -1,8 +1,14 @@
 from hero import *
-from abc import ABC, abstractmethod
 
-class AbstractEffect(ABC):
-    pass
+class AbstractEffect(Hero):
+    def __init__(self, base):
+        self.base = base
+    
+    def get_positive_effects(self):
+        return self.base.get_positive_effects()
+    
+    def get_negative_effects(self):
+        return self.base.get_negative_effects()
 
 class AbstractPositive(AbstractEffect):
     pass
@@ -11,16 +17,77 @@ class AbstractNegative(AbstractEffect):
     pass
 
 class Berserk(AbstractPositive):
-    pass
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['HP'] += 50
+        stats['Strength'] += 7
+        stats['Perception'] -= 3
+        stats['Endurance'] += 7
+        stats['Charisma'] -= 3
+        stats['Intelligence'] -= 3
+        stats['Agility'] += 7
+        stats['Luck'] += 7
+        return stats
+    
+    def get_positive_effects(self):
+        effects = self.base.get_positive_effects()
+        effects.append('Berserk')
+        return effects
 
 class Blessing(AbstractPositive):
-    pass
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] += 2
+        stats['Perception'] += 2
+        stats['Endurance'] += 2
+        stats['Charisma'] += 2
+        stats['Intelligence'] += 2
+        stats['Agility'] += 2
+        stats['Luck'] += 2
+        return stats
+    
+    def get_positive_effects(self):
+        effects = self.base.get_positive_effects()
+        effects.append('Blessing')
+        return effects
 
 class Weakness(AbstractNegative):
-    pass
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] -= 4
+        stats['Endurance'] -= 4
+        stats['Agility'] -= 4
+        return stats
+
+    def get_negative_effects(self):
+        effects = self.base.get_negative_effects()
+        effects.append('Weakness')
+        return effects
 
 class EvilEye(AbstractNegative):
-    pass
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Luck'] -= 10
+        return stats
+
+    def get_negative_effects(self):
+        effects = self.base.get_negative_effects()
+        effects.append('EvilEye')
+        return effects
 
 class Curse(AbstractNegative):
-    pass
+    def get_stats(self):
+        stats = self.base.get_stats()
+        stats['Strength'] -= 2
+        stats['Perception'] -= 2
+        stats['Endurance'] -= 2
+        stats['Charisma'] -= 2
+        stats['Intelligence'] -= 2
+        stats['Agility'] -= 2
+        stats['Luck'] -= 2
+        return stats
+    
+    def get_negative_effects(self):
+        effects = self.base.get_negative_effects()
+        effects.append('Curse')
+        return effects
