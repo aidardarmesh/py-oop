75df0a9663234d66e6612957a8211f6bfb686afb
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-05 00:07:43 +0600

    Cascade for classes

---
+++ week4/chain.py
@@ -0,0 +1,24 @@
+class SomeObject:
+    def __init__(self):
+        self.integer_field = 0
+        self.float_field = 0.0
+        self.string_field = ""
+
+class EventGet:
+    pass
+
+class EventSet:
+    pass
+
+class NullHandler:
+    pass
+
+class IntHandler:
+    pass
+
+class FloatHandler:
+    pass
+
+class StrHandler:
+    pass
+
