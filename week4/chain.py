02de737827c88445823c5ab602bd7029018bc2a1
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-05 00:17:02 +0600

    Added examples

--- week4/chain.py
+++ week4/chain.py
@@ -31,3 +31,23 @@ class FloatHandler(NullHandler):
 
 class StrHandler(NullHandler):
     pass
+
+obj = SomeObject()
+obj.integer_field = 42
+obj.float_field = 3.14
+obj.string_field = "some text"
+chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
+print(chain.handle(obj, EventGet(int)))  # 42
+# chain.handle(obj, EventGet(float))
+# 3.14
+# chain.handle(obj, EventGet(str))
+# 'some text'
+# chain.handle(obj, EventSet(100))
+# chain.handle(obj, EventGet(int))
+# 100
+# chain.handle(obj, EventSet(0.5))
+# chain.handle(obj, EventGet(float))
+# 0.5
+# chain.handle(obj, EventSet('new text'))
+# chain.handle(obj, EventGet(str))
+# 'new text'
