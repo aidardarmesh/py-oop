65fb01c2a4d3a8d6db5bcf03365d045d3783dfe7
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-05 19:44:23 +0600

    Implemented HardLevel

--- week4/abstract.py
+++ week4/abstract.py
@@ -46,6 +46,7 @@ class EasyLevel(AbstractLevel):
 
             return self.objects
 
+
 class MediumLevel(AbstractLevel):
     class Map:
         def __init__(self):
@@ -79,6 +80,49 @@ class MediumLevel(AbstractLevel):
                         if coord == obj[1]:
                             intersect = True
                             coord = (random.randint(1, 6), random.randint(1, 6))
+
+                self.objects.append((obj_name, coord))
+
+            return self.objects
+
+
+class HardLevel(AbstractLevel):
+    class Map:
+        def __init__(self):
+            self._map = [[0 for j in range(10)] for i in range(10)]
+            for i in range(10):
+                for j in range(10):
+                    if i == 0 or j == 0 or i == 9 or j == 9:
+                        # граница карты
+                        self._map[j][i] = -1
+                    else:
+                        # характеристика области (-1 для непроходимой обл.)
+                        self._map[j][i] = random.randint(-1, 8)
+
+        def get_map(self):
+            return self._map
+    
+    class Objects:
+        def __init__(self):
+            # размещаем переход на след. уровень
+            self.objects = [('next_lvl', (5, 5))]
+
+        def get_objects(self, map_obj):
+            # размещаем врагов
+            for obj_name in ['rat', 'snake']:
+                coord = (random.randint(1, 8), random.randint(1, 8))
+                # ищем случайную свободную локацию
+                intersect = True
+                while intersect:
+                    intersect = False
+                    if map_obj.get_map()[coord[0]][coord[1]] == -1:
+                        intersect = True
+                        coord = (random.randint(1, 8), random.randint(1, 8))
+                        continue
+                    for obj in self.objects:
+                        if coord == obj[1]:
+                            intersect = True
+                            coord = (random.randint(1, 8), random.randint(1, 8))
 
                 self.objects.append((obj_name, coord))
 
