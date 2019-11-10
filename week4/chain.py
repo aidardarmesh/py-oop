2f9a16d0345d18f5f7806875917895a849cc432a
Aidar Darmesh <darmesh.aidar@gmail.com>
2019-11-10 18:03:04 +0600

    Late changes

--- week4/chain.py
+++ week4/chain.py
@@ -50,18 +50,3 @@ class StrHandler(NullHandler):
                 char.string_field = event.value
         else:
             return super().handle(char, event)
-
-obj = SomeObject()
-obj.integer_field = 42
-obj.float_field = 3.14
-obj.string_field = "some text"
-chain = IntHandler(FloatHandler(StrHandler(NullHandler)))
-print(chain.handle(obj, EventGet(int)))  # 42
-print(chain.handle(obj, EventGet(float)))  # 3.14
-print(chain.handle(obj, EventGet(str)))  # 'some text'
-chain.handle(obj, EventSet(100))
-print(chain.handle(obj, EventGet(int))) # 100
-chain.handle(obj, EventSet(0.5))
-print(chain.handle(obj, EventGet(float)))  # 0.5
-chain.handle(obj, EventSet('new text'))
-print(chain.handle(obj, EventGet(str)))  # 'new text'
